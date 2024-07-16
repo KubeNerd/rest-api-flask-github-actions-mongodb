e6f2388dd46257516ee4c242767ecd0aa5134319
Vinicius P <vinicius.pereira@gotobiz.com.br>
2024-07-15 21:26:33 -0300

    fix: corrigindo o host que havia ficado vazio

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ api.add_resource(Users, '/users')
 api.add_resource(User, '/user', '/users/<string:cpf>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",debug=True)
