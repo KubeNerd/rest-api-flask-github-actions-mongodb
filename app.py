874575dc2c6fdf7f35790cf3606b6714b8a0c041
rxjsvini <viniciuspoa2@hotmail.com>
2024-07-17 22:12:50 -0300

    feat: melhorias, e ajuste na conexão do banco

--- app.py
+++ app.py
@@ -1,24 +1,43 @@
-from flask import Flask
+from flask import Flask,jsonify
 from flask_restful import Resource, Api
 from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
+
+app.config["MONGODB_SETTINGS"] = {
+        "db": "users",
+        "host": "mongodb",
+        "username":"admin",
+        "password":"MinhaSenha123@Forte!!",
+        "port": 27017,
+    }
+
 api = Api(app)
 db = MongoEngine(app)
 
-class Users(Resource):
-    def get(self):
-        return {"message": "user 1"}
+
+class UserModel(db.Document):
+    cpf = db.StringField(max_length=200, unique=True)
+    email = db.StringField(max_length=200, required=True)
+    first_name = db.StringField(max_length=200, required=True)
+    last_name = db.StringField(max_length=200, required=True)
+    birth_date = db.DateTimeField(required=True)
+
 
 
 class User(Resource):
-    def get(self):
-        return {"message": "test"}
+    def get(self, cpf):
 
-    def post(self, cpf):
         return {"message": "user 1"}
+    
+    def post(self):
+        return {"message":"user 1"}
 
+class Users(Resource):
+    def get(self):
+        return jsonify(UserModel.objects())
 
+    
 api.add_resource(Users, '/users')
 api.add_resource(User, '/user', '/users/<string:cpf>')
 
