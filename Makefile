0340f85267b0db02c8e0fa7e3a359de887fc99cc
rxjsvini <viniciuspoa2@hotmail.com>
2024-07-21 17:03:42 -0300

    testing: adicionando os testes com pytest

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ APP = restapi
 
 test:
 	@flake8 . --exclude .venv
-
+	@pytest -v --disable-warnings
 compose:
 	@docker compose up -d
 	@docker compose down
---
+++ tests/test_application.py
@@ -0,0 +1,12 @@
+import pytest
+from application import create_app
+
+class TestApplication():
+    @pytest.fixture
+    def client():
+        app = create_app('config.MockConfig')
+        return app.test_client()
+    
+    def test_det_users(self, client):
+        response = client.get('/users')
+        assert response.status_code == 200
